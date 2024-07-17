0d9797f290b2b5b3c0aa617cce14d362c1ea3b19
Yash <yash.parmar@einfochips.com>
2024-07-17 12:25:52 +0530

    Add Node.js application code

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
